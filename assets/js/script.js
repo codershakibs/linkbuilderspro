ce4cfb7b59e12d2a5ff206ccacdaec09a8372005
shakib <codershakibs@gmail.com>
2023-12-11 18:15:19 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,18 @@
-/**
- * @Script js for (Template/Project Name)
- *
- * @project     - Project Name
- * @author      - 
- * @created_by  - Asraful Hoque
- * @created_at  - 
- * @modified_by -
- */
-
-
-/**
- * ========================================================
- * this function execute when window properly loaded
- * ===========================================================
- */
-
-$(window).on('load', function () {
-
-    // code should be execute here
-
-});
-
-
-
-/**
- * ========================================================
- * this function execute when DOM element ready 
- * ===========================================================
- */
-
-$(document).ready(function () {
-
-    // testimonial-active
-    $(function () {
-        if ($('.testimonials-active').length) {
-            $(".testimonials-active").owlCarousel({
-                items: 3,
-                margin: 30,
-                nav: true,
-                loop: true,
-                autoplay: true,
-                autoplayTimeout: 2500,
-                animateOut: 'fadeOut',
-                smartSpeed: 2500,
-                navText: ["<img src='assets/img/arrow-left.svg' class='img-fluid' />", "<img src='assets/img/arrow-right-s.svg' class='img-fluid' />"],
-                dots: false,
-                autoplayHoverPause: true,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    767: {
-                        items: 2
-                    },
-                    1200: {
-                        items: 3
-                    }
-                }
-            });
-        }
-    });
-
-
-
-    // wow js init
-    $(function () {
-        var wow = new WOW({
-            animateClass: 'animated',
-            mobile: true
-        });
-        wow.init();
-    });
-
-});
-
-
-
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -84,4 +21,72 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+
+        991: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+
+        667: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+
+        575: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        },
+
+        300: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+    }
+});
+
+
+var swiper = new Swiper(".updateSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+
+        575: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        },
+
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        },
+    }
+
+
+
 });
